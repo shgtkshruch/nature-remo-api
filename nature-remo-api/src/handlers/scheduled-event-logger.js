082b5e131f5cc05416ca0263179b0edf5082253e
shgtkshruch <shgtk.shruch@gmail.com>
2020-08-09 13:50:31 +0900

    Set over and min temperature

--- nature-remo-api/src/handlers/scheduled-event-logger.js
+++ nature-remo-api/src/handlers/scheduled-event-logger.js
@@ -5,39 +5,43 @@
 const fetch = require("node-fetch");
 const accessToken = process.env.NATURE_REMO_ACCESS_TOKEN;
 const AIRCON_ID = process.env.AIRCON_ID;
+const DEVICE_ID = process.env.DEVICE_ID;
+const HOT = parseInt(process.env.HOT, 10);
+const COLD = parseInt(process.env.COLD, 10);
+const AIR_TEMPERATURE = process.env.AIR_TEMPERATURE;
 const BASE_URL = "https://api.nature.global/1";
 
 exports.nightBedRoomAirConditioner = async (event, context) => {
   // All log statements are written to CloudWatch by default. For more information, see
   // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
 
-  // const res = await turnOnAircon(AIRCON_ID, 28);
-  const res = await turnOffAircon(AIRCON_ID);
-  console.dir(res, { depth: 10 });
+  let res;
+
+  const devices = await getDevices();
+  const device = devices.filter((device) => device.id === DEVICE_ID)[0];
+  const temperature = device.newest_events.te.val;
+
+  switch (true) {
+    case (temperature >= HOT):
+      log(`[Temperature ${temperature}℃]: Turn on air conditoner`);
+      res = await turnOnAircon(AIRCON_ID, AIR_TEMPERATURE);
+      break;
+    case (temperature <= COLD):
+      log(`[Temperature ${temperature}℃]: Turn off air conditoner`);
+      res = await turnOffAircon(AIRCON_ID);
+      break;
+    default:
+      res = `[Temperature ${temperature}℃]: It's just the right room temperature`;
+      break;
+  }
+
+  log(res);
 };
+
+const log = (logObj) => (console.dir(logObj, { depth: 10 }) );
 
 const getDevices = async () => {
   const res = await fetch(`${BASE_URL}/devices`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return await res.json();
-};
-
-const getAppliances = async () => {
-  const res = await fetch(`${BASE_URL}/appliances`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return await res.json();
-};
-
-const getSignales = async (applianceId) => {
-  const res = await fetch(`${BASE_URL}/appliances/${applianceId}/signals`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${accessToken}`,
@@ -76,6 +80,26 @@ const turnOffAircon = async (applianceId) => {
       Authorization: `Bearer ${accessToken}`,
     },
     body: params,
+  });
+  return await res.json();
+};
+
+const getAppliances = async () => {
+  const res = await fetch(`${BASE_URL}/appliances`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return await res.json();
+};
+
+const getSignales = async (applianceId) => {
+  const res = await fetch(`${BASE_URL}/appliances/${applianceId}/signals`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
   });
   return await res.json();
 };
